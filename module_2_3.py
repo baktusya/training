0ffd9423bc6438124dc37c5a1811fe2231653be5
baktusya <baktusya@mail.ru>
2024-09-16 19:28:17 +0300

    Осталось подумать как избавиться от нуля в списке

---
+++ module_2_3.py
@@ -0,0 +1,18 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+number = my_list[0]
+index = 1
+while index <= len(my_list):
+    if number >= 0:
+        print(number)
+        number = my_list[index]
+        index += 1
+        continue
+    else:
+        break
+
+
+
+
+
+
+
