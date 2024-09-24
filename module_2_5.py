788ef8ccc4a099f79d4f403e41b4de5421cdd275
baktusya <baktusya@mail.ru>
2024-09-24 22:41:08 +0300

    Задание выполнено

---
+++ module_2_5.py
@@ -0,0 +1,15 @@
+def get_matrix(n, m, value):
+    matrix = list()
+    for i in range(n):
+        stroka = list()
+        for j in range(m):
+            stroka.append(value)
+        matrix.append(stroka)
+    return matrix
+
+example1 = get_matrix(2, 3, 7)
+example2 = get_matrix(6, 2, 12)
+example3 = get_matrix(5, 4, 8)
+print(example1, sep = "\n")
+print(example2)
+print(example3)
