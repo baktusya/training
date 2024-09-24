9c72eeb38598754528d2834ab529cde8e87e8691
baktusya <baktusya@mail.ru>
2024-09-24 22:42:34 +0300

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -10,6 +10,6 @@ def get_matrix(n, m, value):
 example1 = get_matrix(2, 3, 7)
 example2 = get_matrix(6, 2, 12)
 example3 = get_matrix(5, 4, 8)
-print(example1, sep = "\n")
+print(example1)
 print(example2)
 print(example3)
