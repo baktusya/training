19f3c8e582a779c55ad7fef13bf11a0a157263d4
baktusya <baktusya@mail.ru>
2024-09-28 18:49:59 +0300

    Задание выполнено

--- module_3_1.py
+++ module_3_1.py
@@ -1,8 +1,19 @@
 calls = 0
 def count_calls ():
-    calls += 1
+    calls += 1    # количество вызовов этих двух функций
 def string_info(string):
-    count_calls
+    global calls
+    calls += 1
+    string = tuple([len(string), string.upper(), string.lower()])
+    return string
 def is_contains(string, list_to_search):
-    count_calls
+    global calls
+    calls += 1
+    list_to_search = [word.lower() for word in list_to_search]
+    string = string.lower()
+    return string in list_to_search
+
+print(string_info('колонада'))
+print(string_info('банан'))
+print(is_contains('вода', ['куКУруза', 'КОлодец', 'воДа', 'аПельСин']))
 print(calls)
