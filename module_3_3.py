6957dccec2e1b3ec7f3a2af6dd7a07719df90e44
baktusya <baktusya@mail.ru>
2024-09-29 11:53:08 +0300

    Исправлено

--- module_3_3.py
+++ module_3_3.py
@@ -4,4 +4,12 @@ def print_params(a = 1, b = 'строка', c = True):
 values_list = [5, 'блок', False]
 values_list_2 = ['олово', 5.67 ]
 values_dict = {'a': 56, 'b': 34.6, 'c': 'лось' }
+
+print_params()
+print_params(5, False, 'слово')
+print_params('фонарь', 45.7)
+print_params(b = 25)
+print_params(c=[1, 2, 3])
+print_params(*values_list)
+print_params(**values_dict)
 print_params(*values_list_2, 42)
