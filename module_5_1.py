6203e570f17fc2f3ea06e9d8ffc4ab7105c2892c
baktusya <baktusya@mail.ru>
2024-10-20 20:25:43 +0300

    Задание выполнено

--- module_5_1.py
+++ module_5_1.py
@@ -1,20 +1,20 @@
 class House:
-  def __init__(self, name, number_of_floors):
-      self.name = name
-      self.number_of_floors = number_of_floors
-      self.go_to()
-      
-    def go_to(new_floor):
-      nonlocal number_of_floors
-      i = 1
-      for i in range(new_floor):
-        if new_floor > number_of_floors < 1:
-          print('Такого этажа не существует')
-        else:
-          i += 1
-          print(i)  
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
 
-            
+    def go_to(self, new_floor):
+        number_of_floors = self.number_of_floors
+        i = 1
+        for i in range(new_floor):
+            if new_floor > number_of_floors or number_of_floors < 1:
+                print('Такого этажа не существует')
+                break
+            else:
+                i += 1
+                print(i)
+
+
 h1 = House('ЖК Солнечный', 12)
 h2 = House('ЖК Лофт', 7)
 h1.go_to(4)
