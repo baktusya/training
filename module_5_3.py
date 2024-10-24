d8222a4fb863aa63e2adaf5863e2a9a6cc5a4da5
baktusya <baktusya@mail.ru>
2024-10-24 22:20:38 +0300

    Добавлены комментарии в код

--- module_5_3.py
+++ module_5_3.py
@@ -16,7 +16,7 @@ class House:
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
-    def __eq__(self, other):
+    def __eq__(self, other): # почему-то выдаёт сообщение про номер этажа и None в консоль
         if isinstance(other, int):
             if self.number_of_floors == other:
                 return True
@@ -77,14 +77,14 @@ print(h1)
 print(h2)
 print(h1 == h2)  # __eq__
 h1 = h1 + 10  # __add__
-print(h1)
+print(h1) # выдаёт просто число из переменной выше, а не вызывает функцию __str__ с новым значением переменной (Должно быть: Название: ЖК Солнечный, кол-во этажей: 22)
 print(h1 == h2)
 h1 += 10  # __iadd__
-print(h1)
-#h2 = 10 + h2  # __radd__
-#print(h2)
-#print(h1 > h2)  # __gt__
-#print(h1 >= h2)  # __ge__
-#print(h1 < h2)  # __lt__
-#print(h1 <= h2)  # __le__
-#print(h1 != h2)  # __ne__
+print(h1) # выдаёт просто число из переменной выше, а не вызывает функцию __str__ с новым значением переменной (Должно быть: Название: ЖК Солнечный, кол-во этажей: 32)
+h2 = 10 + h2  # __radd__
+print(h2) # выдаёт просто число из переменной выше, а не вызывает функцию __str__ с новым значением переменной (Должно быть: Название: ЖК Солнечный, кол-во этажей: 17)
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
