6c56dbda226202768552f247e63c8d73922648e7
baktusya <baktusya@mail.ru>
2025-01-06 02:44:39 +0300

    Задание выполнено

---
+++ module_8_1.py
@@ -0,0 +1,10 @@
+def add_everything_up(a,b):
+    try:
+        result = a + b
+    except TypeError:
+        return f"{a}{b}"
+    return round(result, 3)
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
