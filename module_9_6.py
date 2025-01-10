ef49de3223b652b4e80211fe9b9ec8d23ed0ffaf
baktusya <baktusya@mail.ru>
2025-01-10 16:52:51 +0300

    Исправлены недочеты

--- module_9_6.py
+++ module_9_6.py
@@ -1,11 +1,12 @@
 import itertools
 
 def all_variants(text):
-    for i in range(1, len(text) + 1):
-        for value in itertools.combinations(text, i):
-            yield ''.join(value)
+    n = len(text)
+    for length in range(1, n + 1):
+        for start in range(n - length + 1):
+            substring = text[start:start + length]
+            yield substring
 
 a = all_variants("abc")
 for i in a:
     print(i)
-
