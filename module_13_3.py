a9325c1f69b36b33bab5bbc50bb56a434ce5b16b
baktusya <baktusya@mail.ru>
2025-01-18 04:08:27 +0300

    Задание выполнено

--- module_13_3.py
+++ module_13_3.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = ''
+api = 'мой токен'
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
